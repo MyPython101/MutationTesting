5c474807fdcd3bab357c5a05773733204c5d0a39
TrucDev <jackyhuynh87@gmail.com>
2022-04-02 15:23:54 -0400

    Update test_automation.py

--- ROOT_AUTOMATION/test_automation.py
+++ ROOT_AUTOMATION/test_automation.py
@@ -1,2 +1,22 @@
 from subprocess import call
-call(["ls", "-l"])
+import os
+
+# can also use os but not recommend
+
+# Step 1: Initializing a session ("tutorial.sqlite")
+call(["cosmic-ray", "init", "tutorial.toml", "tutorial.sqlite"], shell=True)
+
+# Check that the test suite passes on unmutated code (OPTIONAL)
+call(["cosmic-ray", "--verbosity=INFO", "baseline", "tutorial.toml"], shell=True)
+
+# Examining the session with cr-report  (OPTIONAL)
+call(["cosmic-ray", "tutorial.sqlite", "--show-pending"], shell=True)
+
+# Step 2: Actual execute the test script:
+call(["cosmic-ray", "exec", "tutorial.toml", "tutorial.sqlite"], shell=True)
+
+# Examining the session with cr-report (OPTIONAL)
+call(["cosmic-ray", "tutorial.sqlite", "--show-pending"], shell=True)
+
+# Creating an HTML report:
+call(["cr-html", "tutorial.sqlite", ">", "report.html"], shell=True)
